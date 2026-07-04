e0d1c994b64dc8caea302a5ce3324838489ab0d9
Bryan Gonzalez <bryan.gonzalez1020.bg@gmail.com>
2026-07-03 21:48:34 -0500

    Initiated Frontend

---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,13 @@
+const { Link } = require("react-router-dom");
+
+const Nav = () => {
+  return (
+    <div className="nav">
+      <Link to="/">
+        <h3>Fragrance</h3>
+      </Link>
+    </div>
+  );
+};
+
+export default Nav;
